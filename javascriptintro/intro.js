28d172634643cf9b8286880789f6c105fa2afbb9
Adedayo Agboola <kreativedayo@gmail.com>
2022-06-22 07:58:24 +0100

    My Zuri Frontend Assignments
    My Zuri Frontend Assignments

---
+++ javascriptintro/intro.js
@@ -0,0 +1,9 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+
+const myName ="Adedayo Agboola";
+let myHeight = "1.66";
+let myCountry = "Nigeria";
+let theOutput = "My name is " + myName + ", height is " + myHeight + "m and I am from " + myCountry + ".";
+
+console.log(theOutput);
+// document.write(output);
